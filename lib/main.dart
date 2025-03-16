d474bcc8bba700ca820dd911068c5b04b53b4e38
potados99 <potados99@gmail.com>
2025-03-16 11:46:27 +0900

    feat: 당겨서 새로고침하면서 empty/error/loading은 가운데에 표시해요

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:haptic_feedback/haptic_feedback.dart';
 import 'package:my_first_flutter/post.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -41,19 +42,65 @@ class MyHomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final posts = ref.watch(postsProvider);
 
+    Future<void> refresh() async {
+      if (await Haptics.canVibrate()) {
+        Haptics.vibrate(HapticsType.light);
+      }
+      try {
+        await ref.refresh(postsProvider.future);
+
+        if (await Haptics.canVibrate()) {
+          Haptics.vibrate(HapticsType.success);
+        }
+      } catch (e) {
+        if (await Haptics.canVibrate()) {
+          Haptics.vibrate(HapticsType.error);
+        }
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(title),
       ),
-      body: RefreshIndicator(
-        onRefresh: () => ref.refresh(postsProvider.future),
-        child: ListView(
-          children: posts.valueOrNull?.map((post) => ListTile(
-            title: Text(post.body),
-            subtitle: Text(post.date),
-          )).toList() ?? const [],
-        ),
+      body: Stack(
+        children: [
+          RefreshIndicator(
+            onRefresh: refresh,
+            child: ListView(
+              children: switch (posts) {
+                AsyncData(:final value) when value.isNotEmpty => value
+                    .map(
+                      (post) => ListTile(
+                        title: Text(post.body),
+                        subtitle: Text(post.date),
+                      ),
+                    )
+                    .toList(),
+                _ => [],
+              },
+            ),
+          ),
+          if (posts is AsyncLoading)
+            const Center(
+              child: CircularProgressIndicator(),
+            ),
+          if (posts is AsyncError)
+            const Center(
+              child: Text(
+                'Error loading posts',
+                style: TextStyle(color: Colors.red, fontSize: 18),
+              ),
+            ),
+          if (posts case AsyncData(:final value) when value.isEmpty)
+            const Center(
+              child: Text(
+                'No posts available',
+                style: TextStyle(color: Colors.grey, fontSize: 18),
+              ),
+            ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => ref.refresh(postsProvider.future),
